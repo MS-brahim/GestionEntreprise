b4e14a63f3ac9cd49acc567a40a4e377a1615b1a
Youssef Abada <youssef.abada.x@gmail.com>
2020-04-02 10:42:18 +0100

    load the geolocation map dynamically depending on the givin adress

--- Youssef Abada/Website/public/script.js
+++ Youssef Abada/Website/public/script.js
@@ -1,19 +1,16 @@
 /*jshint esversion: 6 */
 $(document).ready(function () {
-    $("#show").click(function () {
-        $(this).hide(200);
-        $("#hide").show(200);
-    });
-    $("#hide").click(function () {
-        $(this).hide(200);
-        $("#show").show(200);
-    });
     if (window.location.pathname === "/")
         loadEntreprises();
     if (window.location.pathname === "/department.html") {
         loadDepartmentInfo();
     }
 });
+
+
+function showhide(){
+    $(this).toggle();
+}
 
 function loadDepartmentInfo() {
     let request = new XMLHttpRequest();
@@ -41,14 +38,15 @@ function loadEntreprises() {
                 enter += "<div class='ent'><h3 class='ent-name'>" + entreprises[e].name + "</h3>";
                 enter += "<div class='ent-body'><p class='ent-disc'>" + entreprises[e].discription + "</p>";
                 enter += "<p id='show' class='text-primary show-more' data-toggle='collapse' data-target='#detail" + entreprises[e].id + "'>Plus d'info ▼</p>";
-                enter += "<div id='detail" + entreprises[e].id + "' class='collapse'><div class='ent-detail'><div class='map'>";
-                enter += "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3297.581679627881!2d-6.586231385013859!3d34.259216414242374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda759e405b8c3a1%3A0x9be254e43555ea5f!2sKenitra%20mall!5e0!3m2!1sen!2sma!4v1585410711027!5m2!1sen!2sma' width='100%' height='100%' frameborder='0' style='border:0;' allowfullscreen='' aria-hidden='false' tabindex='0'></iframe></div>";
+                enter += "<div id='detail" + entreprises[e].id + "' class='collapse'><div class='ent-detail'>";
+                enter += "<div style='width: 100%; height:150px;'>";
+                let adress = entreprises[e].location.replace(/ /g, "+").replace(/,/g, "");
+                enter += '<iframe width="100%" height="170" frameborder="0" src="https://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=' +adress+ '&z=14&output=embed"></iframe></div>';
                 enter += "<p class='text-primary'></p><div class='detail-text'><p>" + entreprises[e].location + "</p>'<p class='text-primary'></p><div class='detail-text'>";
                 enter += "<p><span class='text-primary'>" + entreprises[e].deprartments.length + "</span> déprartments</p></div></div>";
                 enter += "<p id='hide' class='text-primary show-more' data-toggle='collapse' data-target='#detail" + entreprises[e].id + "'>Montrer moins ▲</p></div>";
                 enter += `</div><button onclick="showDepatements(${entreprises[e].id})" data-toggle="modal" data-target="#showDepatments" type="button" class="btn btn-primary btn-block" style="border-radius: 8px; font-size: .9em; background-color: #5599FF; color: white;">Afficher / ajouter des départements</button></div></div>`;
                 document.getElementById("entreprises").innerHTML += enter;
-                //modalAddDep(${entreprises[e].id}, '${entreprises[e].name}')
             }
         }
     };
