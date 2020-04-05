52d87ce995e4ac44d8ca3f8fc65f07a7775d6278
Youssef Abada <youssef.abada.x@gmail.com>
2020-04-05 01:00:12 +0100

    helping zakaria

--- zakariae/website/public/js/search.js
+++ zakariae/website/public/js/search.js
@@ -1,16 +1,45 @@
-$(document).ready(function() {
-    $('#search').keyup(function() {
-        $('#result').html('');
-        var searchField = $('#search').val();
-        var expression = new RegExp(searchField, "i");
-        $.getJSON('entreprises.json', function(data) {
-            $.each(data, function(key, value) {
-                if(value.name.search(expression) != -1 )
-                {
-                   $('#result').append() 
-                }
-            })
-        })
+/*jshint esversion: 6 */
+loadSearchResult();
 
-    })
-});
+function ajaxRequest(method, url, callback, data) {
+    let request = new XMLHttpRequest();
+    request.onload = function () {
+        let res = null;
+        if (this.status == 200) {
+            if (method === "GET")
+                res = JSON.parse(this.responseText);
+            callback(res);
+        }
+    };
+    request.open(method, url, true);
+
+    if (method === "POST") {
+        data = JSON.stringify(data);
+        request.setRequestHeader("Content-type", "application/json");
+        request.send(data);
+    }
+    if (method === "GET") {
+        request.send();
+    }
+}
+
+function searchSalary(event, input) {
+    if (event.keyCode === 13) {
+        window.open('/search/?name=' + input.value, "_self");
+    }
+}
+
+function loadSearchResult() {
+    ajaxRequest("GET", "/api/search", res => {
+        document.getElementById("salaryInfo").innerHTML = "";
+        if (res.length > 0) {
+            document.getElementById("searchFor").innerHTML = "Vous cherchez <span class='text-primary'>" + res[0].name.toUpperCase() + "</span>";
+            for (let s in res) {
+                document.getElementById("salaryInfo").innerHTML += '<tr><td>' + res[s].matricule + '</td><td>' + res[s].entrepriceName + '</td><td>' + res[s].departementName + '</td><td>' + res[s].name + '</td><td>' + res[s].lastName + '</td><td>' + res[s].age + '</td><td>' + res[s].salaire + '</td></tr>';
+            }
+        } else {
+            document.getElementById("searchFor").innerHTML = "Aucun résultat trouvé!".toUpperCase();
+        }
+    });
+}
+
